57dcda0dc76cc98e5b516fe3576f91e88d7e6585
“lijunjie2232” <2533584225@qq.com>
2023-05-26 11:18:23 +0800

    hard learning

--- threadtry/RunnableDemo.java
+++ threadtry/RunnableDemo.java
@@ -1,24 +1,26 @@
-class MyThread implements Runnable{
+class MyRunnableThread implements Runnable {
     private String title;
-    public MyThread(String title) {
+
+    public MyRunnableThread(String title) {
         this.title = title;
     }
-    
+
     @Override
     public void run() {
-    for (int i = 10; i > 0; i --){
-        System.out.println(title + " : " + i);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 10; i > 0; i--) {
+            System.out.println(title + " : " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-    }
 }
+
 public class RunnableDemo {
     public static void main(String[] args) {
-        Thread t = new Thread(new MyThread("Demo"));
+        Thread t = new Thread(new MyRunnableThread("Demo"));
         t.start();
     }
 }
--- threadtry/ThreadDemo.java
+++ threadtry/ThreadDemo.java
@@ -1,30 +1,33 @@
 class MyThread extends Thread {
     private String title;
+
     public MyThread(String title) {
         this.title = title;
     }
+
     @Override
     public void run() {
-    for (int i = 10; i > 0; i --){
-        System.out.println(title + " : " + i);
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        for (int i = 10; i > 0; i--) {
+            System.out.println(title + " : " + i);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
-    }
 }
-public class ThreadDemo{
-    public static void main(String[] args) throws Exception{
-        MyThread [] thread = new MyThread[3];
-        for (int i = 0; i < thread.length; i ++){
+
+public class ThreadDemo {
+    public static void main(String[] args) throws Exception {
+        MyThread[] thread = new MyThread[3];
+        for (int i = 0; i < thread.length; i++) {
             thread[i] = new MyThread("thread" + i);
         }
         thread[0].start();
         // thread[0].start();// throw java.lang.IllegalThreadStateException
         thread[0].join();
         System.out.println("finished");
-        
+
     }
 }
