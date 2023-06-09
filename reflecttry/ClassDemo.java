0f42f3263a504c5f4e4f91386902d1458bba00d3
“lijunjie2232” <2533584225@qq.com>
2023-06-09 10:41:56 +0800

    hard learning

--- reflecttry/ClassDemo.java
+++ reflecttry/ClassDemo.java
@@ -1,5 +1,7 @@
 package reflecttry;
 
+import java.lang.reflect.*;
+
 public class ClassDemo {
     public static void main(String[] args) throws Exception {
         String str = new String();
@@ -8,11 +10,26 @@ public class ClassDemo {
         System.out.println(java.lang.String.class);
         System.out.println(java.lang.Class.forName("java.lang.String"));
         Class<? extends String> strClass = java.lang.String.class;
-        System.out.println(strClass.getDeclaredConstructor().newInstance().getClass());
+        System.out.println(strClass.getDeclaredConstructor(byte[].class).newInstance("newInstance".getBytes()));
         System.out.println(strClass.getPackage().getName());
         System.out.println(strClass.getSuperclass());
-        System.out.println("getInterfaces:");
+        System.out.println("****************** getInterfaces:");
         for (Class<?> cls : strClass.getInterfaces())
             System.out.println(cls);
+        System.out.println("****************** getConstructors");
+        for (Constructor<?> cons : strClass.getConstructors())
+            System.out.println(cons);
+        System.out.println(strClass.getConstructors().length);
+
+        System.out.println("****************** getMethods:");
+        for (Method m : strClass.getMethods())
+            System.out.println(m);
+        System.out.println("****************** getMethods numbers: " + strClass.getMethods().length);
+        
+        System.out.println("****************** getDeclaredMethods:");
+        for (Method m : strClass.getDeclaredMethods())
+            System.out.println(m);
+        System.out.println("****************** getDeclaredMethods numbers: " + strClass.getDeclaredMethods().length);
+
     }
 }
